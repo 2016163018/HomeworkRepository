3597e510c646fa4e1554d69b362e6fdea53a2294
2016163018 <2016163018@yonsei.ac.kr>
2022-06-01 18:51:54 +0900

    Implement main page

--- LAB6/public/index.js
+++ LAB6/public/index.js
@@ -1,126 +1,45 @@
-let isLoading = false;
-let category = "all";
-let keyword = "";
-let data = new Proxy([], {
-  set: (target, property, value) => {
-    target[property] = value;
-    const filteredData = filterData(target, category, keyword);
-    makeItem(filteredData);
-    return true;
-  },
-});
-
-const fetchProduct = async () => {
-  if (isLoading) {
-    return;
-  }
-
-  isLoading = true;
-  const res = await fetch("product.json");
-  const json = await res.json();
-
-  Array.prototype.push.call(data, ...json);
-
-  makeCategory();
-  isLoading = false;
-};
-
-const useSelect = () => {
-  let selectedItem;
-
-  const itemWrapper = document.querySelector(".item-wrapper");
-  itemWrapper.addEventListener("click", (event) => {
-    const itemElement = event.target.closest(".item");
-
-    if (!itemElement || itemElement === selectedItem) {
-      return;
-    }
-
-    if (selectedItem) {
-      selectedItem.classList.remove("item__selected");
-    }
-
-    itemElement.classList.add("item__selected");
-    selectedItem = itemElement;
-  });
-};
-
-const makeCategory = () => {
-  const categorySelect = document.querySelector("#category-select");
-  const categories = new Set(data.map((item) => item.category));
-
-  const categoryHtml = Array.from(categories).reduce(
-    (prev, category) =>
-      prev + `<option value='${category}'>${category}</option>`,
-    '<option value="all">all</option>'
-  );
-
-  categorySelect.innerHTML = categoryHtml;
-  categorySelect.value = category;
-};
-
-const makeItem = (data) => {
-  const itemWrapper = document.querySelector(".item-wrapper");
-  const items = data.reduce(
-    (prev, item) =>
-      prev +
-      `
-            <div class="item">
-                <img class="item-image" src="${item.image}" />
-                <div class="item-name">${item.name}</div>
-                <div class="item-desc">${item.desc}</div>
-            </div>
-            `,
-    ""
-  );
-
-  itemWrapper.innerHTML = items;
-};
-
-const filterData = (data, category, keyword) => {
-  return data.filter((item) => {
-    const isMatchedCategory = category === "all" || item.category === category;
-    const isMatchedKeyword =
-      item.name.includes(keyword) || item.desc.includes(keyword);
-    return isMatchedCategory && isMatchedKeyword;
-  });
-};
-
 const useFilter = () => {
   const categorySelect = document.querySelector("#category-select");
   const searchForm = document.querySelector(".search-form");
 
   categorySelect.addEventListener("change", ({ target }) => {
-    category = target.value;
-    const filteredData = filterData(data, category, keyword);
-    makeItem(filteredData);
+    const category = target.value;
+    const search = new URLSearchParams(window.location.search);
+
+    if (category === "all") {
+      search.delete("category");
+    } else {
+      search.set("category", category);
+    }
+
+    window.location.search = search.toString();
   });
 
   searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    keyword = event.target.keyword.value;
-    const filteredData = filterData(data, category, keyword);
-    makeItem(filteredData);
+    const keyword = event.target.keyword.value;
+    const search = new URLSearchParams(window.location.search);
+
+    search.set("keyword", keyword);
+
+    window.location.search = search.toString();
   });
 };
 
-const useInfiniteScroll = () => {
-  const target = document.querySelector(".observe-target");
-  const io = new IntersectionObserver(([entry]) => {
-    if (!entry.isIntersecting) {
-      return;
+const useSelect = () => {
+  const category = new URLSearchParams(window.location.search).get("category");
+
+  if (!category) {
+    return;
+  }
+
+  const options = document.querySelectorAll("option");
+  options.forEach((option) => {
+    if (option.value === category) {
+      option.setAttribute('selected', 'selected');
     }
-    fetchProduct();
   });
-  io.observe(target);
 };
 
-const init = async () => {
-  fetchProduct();
-  useSelect();
-  useFilter();
-  makeItem(data);
-  useInfiniteScroll();
-};
-
-init();
+useFilter();
+useSelect();
--- LAB6/routes/index.js
+++ LAB6/routes/index.js
@@ -1,8 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const router = express.Router();
 
-router.get('/', function(_, res) {
-  res.render('index');
+const db = new sqlite3.Database('./db/product.db');
+
+router.get('/', function (req, res) {
+  const category = req.query.category;
+  const keyword = req.query.keyword || "";
+
+  const where = `${`WHERE product_title LIKE "%${keyword}%"` || "" }${category && `AND product_category="${category}"` || ""}`
+  const query = 'SELECT * FROM product ' + where
+
+  db.all(query, (err, rows) => {
+    console.log(err);
+    res.render('index', {items:rows, category, keyword});
+  })
 });
 
 router.get('/login', function(_, res) {
