6c61e74808b09185cfbaabd76e1773a8636ec246
2016163018 <2016163018@yonsei.ac.kr>
2022-06-01 16:50:39 +0900

    Implement express base setting

---
+++ LAB6/app.js
@@ -0,0 +1,32 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use(function(_, _, next) {
+  next(createError(404));
+});
+
+const port = '3000';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
